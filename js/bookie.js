77a38afe9825b838dd4589d075b49e2e633190c9
Estemaux <esteban.cuellaraguilar.us@gmail.com>
2022-02-02 18:12:45 -0500

    Update bookie.js

--- js/bookie.js
+++ js/bookie.js
@@ -7,3 +7,17 @@ let y = x[x].index; //Number of user ID's
 print(y.shape);
 ratings = ratings[ratings['user_id'].isin(y)];
 }
+
+var http = require('http');
+var hostname = 'localhost';
+var port = 2022;
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Welcome to Bookie!');
+  });
+
+server.listen (port, hostname (){
+    console.log('server running at ${hostname}:${port}')
+});
+
