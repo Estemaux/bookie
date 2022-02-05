ac2aab71f76a6f81bebc7c827daa0dec63c15d23
Estemaux <esteban.cuellaraguilar.us@gmail.com>
2022-02-05 10:38:22 -0500

    Comment out

--- js/bookie.js
+++ js/bookie.js
@@ -1,25 +1,25 @@
 // Extracting users and ratings from more than 50 users
 
-document.querySelector("#ratings").onclick = function()
-{
-let x = ratings['user_id'].value_counts() > 50;
-let y = x[x].index; //Number of user ID's
-print(y.shape);
-ratings = ratings[ratings['user_id'].isin(y)];
-}
+// document.querySelector("#ratings").onclick = function()
+// {
+// let x = ratings['user_id'].value_counts() > 50;
+// let y = x[x].index; //Number of user ID's
+// print(y.shape);
+// ratings = ratings[ratings['user_id'].isin(y)];
+// }
 
-var http = require('http');
-var hostname = 'localhost';
-var port = 2022;
+// var http = require('http');
+// var hostname = 'localhost';
+// var port = 2022;
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Welcome to Bookie!');
-  });
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('Welcome to Bookie!');
+//   });
 
-server.listen (port, hostname (){
-    console.log('server running at ${hostname}:${port}')
-});
+// server.listen (port, hostname (){
+//     console.log('server running at ${hostname}:${port}')
+// });
 
 function euclideanDistance (user1, user2) {
   const n = _.size(user1.reviews)
